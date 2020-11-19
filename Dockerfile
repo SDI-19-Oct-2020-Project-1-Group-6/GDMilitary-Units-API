10a40366c8ab16c9c9f7eff3fd1b5091629d6e86
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-19 12:19:03 -0800

    api complete?

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:current-alpine
+RUN mkdir /app
+COPY ./ /app
+WORKDIR /app
+RUN npm install
+
+CMD ["node","."]
---
+++ index.js
@@ -0,0 +1,164 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+var pgp = require('pg-promise')(/* options */)
+var dbuser = process.env.DBUSER || "postgres"
+var dbpass = process.env.DBPASS || "postgres"
+var address = process.env.DBADDRESS || "localhost"
+var dbport = process.env.DBPORT || 4445
+var dbname = process.env.DBNAME || "postgres"
+var db = pgp(`postgres://${dbuser}:${dbpass}@${address}:${dbport}/${dbname}`)
+var port = 4444;
+app.use(bodyParser.json())
+// const UNIT_ARRAY= async (filter)=>{
+//     try {
+//         const result = await db.query(`SELECT * FROM units WHERE ${filter} ORDER BY id ASC`);
+//         resolve(result);
+//     }catch(e) {
+//         console.log(e.message);
+//     }
+//     reject();
+    
+// }
+// const FIRST_UNIT= async (filter) => {
+//     try {
+//         const result = await db.query(`SELECT * FROM units WHERE ${filter} ORDER BY id ASC`);
+//         if (result.length > 0) {
+//             resolve(result[0]);
+//         }else{
+//             resolve(undefined)
+//         }
+//     }catch(e) {
+//         console.log(e.message);
+//     }
+//     reject();
+// }
+// const NEW_UNIT = async (name,location,size) => {
+//     try {
+//         const result = await db.query('INSERT INTO units (name,location,size) VALUES ($1,$2,$3) RETURNING *',[name,location,size]);
+//         if (result.length > 0) {
+//             resolve(result[0]);
+//         }
+//     }catch(e) {
+//         console.log(e.message);
+//     }
+//     reject();
+// }
+// const AFSC_ENTRY = async (unitid,afsc) => {
+//     try {
+//         const result = await db.query('INSERT INTO unit_afscs (unit_id,afsc_id) VALUES ($1,$2) ON CONFLICT (unit_id,afsc_id) DO NOTHING RETURNING *',[unitid,afsc]);
+//         if (result.length > 0) {
+//             resolve(result[0]);
+//         }
+//     }catch(e) {
+//         console.log(e.message);
+//     }
+//     reject();
+    
+// }
+// const NEW_EMAIL=async (data)=> {
+//     try {
+//         if (!data.date) {
+//             data.date = new Date().toISOString();
+//         }  
+//         //TODO strip inputs to avoid injection
+//         const result = db.result('INSERT INTO emails (sender,recipient,subject,message, date) VALUES ($1,$2,$3,$4,$5)',[data.sender,data.recipient,data.subject,data.message,data.date])
+//         return result.rowCount > 0;
+//     }catch(e) {
+//         console.log(e.message);
+//     }
+//     return false;
+// }
+function statusMessage(responder,code,message, result=undefined) {
+   // console.log('sending status: ',code,message,result);
+    responder.status(code).json({
+        code:code,
+        message:message,
+        result:result
+    });
+}
+
+app.get('/units',(req,resp)=>{
+    db.query(`SELECT * from getUnits();`)
+        .then(data=>statusMessage(resp,200,'success',data))
+        .catch(()=>statusMessage(resp,500,"Failed to get unit data"));
+})
+app.get('/units/:unitName',(req,resp)=> {
+    // lets go bobby tables
+    db.query('SELECT * from getUnit($1);',[req.params.unitName])
+     .then(data=>{
+         if (!Array.isArray(data) || data.length<1) {
+             statusMessage(resp,404,'no unit found')
+         }else{
+            statusMessage(resp,200,'success',data)
+         }
+     })
+     .catch(()=>statusMessage(resp,500,"Error while finding unit"));
+})
+app.post('/units',(req,resp)=>{
+    const {name,location,size,afscs} = req.body;
+    if (name && location && size) {
+        db.query('SELECT * from addUnit($1,$2,$3,$4)',[
+                name,
+                location,
+                size,
+                (Array.isArray(afscs) && afscs.length > 0)?afscs:null
+            ])
+            .then(unit=>statusMessage(resp,200,'success',unit))
+            .catch(error=>{
+                console.log(error.message);
+                statusMessage(resp,500,'failed to process provided data',req.body);
+            })
+    }else{
+        statusMessage(resp,400,'Missing required data');
+    }
+    
+})
+app.delete('/units/:unitName/:afsc',(req,resp)=>{
+    let unit = db.one("SELECT id FROM getUnit($1)",req.params.unitName);
+    db.result('DELETE FROM units_afscs where unit_id=$1 AND afsc_id=$2',[unit.id,req.params.afsc])
+        .then(result=>{
+            if (result.rowCount > 0) {
+                statusMessage(resp,200,'successfully removed afsc from unit');
+            }else{
+                statusMessage(resp,500,'successfully removed afsc from unit');
+            }
+        }).catch(error=>{
+            console.log(error.message);
+            statusMessage(resp,500,'error while removing afsc from unit');
+        })
+});
+app.delete('/units/:unitName',(req,resp)=>{
+    db.result('DELETE FROM units where name=$1',[req.params.unitName])
+    .then(result=>{
+        if (result.rowCount > 0) {
+            statusMessage(resp,200,'successfully removed '+result.rowCount+' units');
+        }else{
+            statusMessage(resp,200,'failed to remove unit '+req.params.unitName);
+        }
+    }).catch(error=>{
+        console.log(error.message); 
+        statusMessage(resp,500,'Server error while attempting to remove unit '+req.params.unitName);
+    })
+    //statusMessage(resp,500,'failed to process request');
+})
+app.patch('/units/:unitName',(req,resp)=>{
+    //build query for update...also let bobby tables in
+    let query = 'UPDATE units SET name='+req.params.unitName;
+    query += (req.body.location)?' location=\''+req.body.location+'\'':'';
+    query += (req.body.size)?' size='+req.body.size:'';
+    db.result(query).then(result=>{
+        if (result.rowCount>0) {
+            if (Array.isArray(req.body.afscs) && req.body.afscs.length>0) {
+                db.query("SELECT addAFSCStoUnit($1,$2)",req.params.unitName,req.body.afscs)
+            }
+            statusMessage(resp,200,'success');
+        }
+        statusMessage(resp,400,'Cannot find unit with the name '+req.params.unitName);
+    }).catch(error=>{
+        console.log(error.message);
+        statusMessage(resp,500,'Server error while attempting to update unit afscs');
+    })
+    //statusMessage(resp,500,'failed to process patch request');
+})
+app.listen(port,()=>console.log("Listening on port 4444"))
